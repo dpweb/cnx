ba3b14d43a1c838da47d40c2a73f6edceb2cb826
C Borkert <success@dpsw.info>
2013-07-03 18:51:35 -0500

    fix

--- cnx.js
+++ cnx.js
@@ -1,24 +1,27 @@
 var http = require('request'), cp = require('child_process'), mysql = require('mysql'),
-url = require('url'), mongoskin = require('mongoskin'), redis = require('redis');
+mongoskin = require('mongoskin'), redis = require('redis');
 
 var cnx = {
     query: function(c, q, cb){
         if(!cb) cb = console.log;
         this[c.split(':')[0]](c, q, cb);
     },
-    http: function(u, p, cb){
-        !p ? http(u, cb) : http.post(u, p, function(e, r){cb(r.body)});
+    http: function(u, q, cb){
+        http(u, cb);
     },
-    cmd: function(u, p, cb){
-        cp.exec(u.replace('cmd://',''), cb);
+    httpp: function(u, q, cb){
+        http.post(u.replace('httpp','http'), q, cb);
+    },
+    cmd: function(u, p){
+        cp.exec(u.replace('cmd://',''), p);
     },
     mysql: function(u, p, cb){
         var cl = mysql.createConnection(u);
         !p ? cb(cl) : cl.query(p, cb);
     },
     sqlserver: function(u, p, cb){
-        var ss = url.parse(u), userpass = ss.auth.split(':');
-        var s = ['sqlcmd -U ', userpass[0], '@', host, ' -S ', host, ' -P ', c.pass, ' -d ', ss.path.replace('/',''),
+        var ss = u.match(/(.*)\:\/\/([^\:]*)\:([^@]*)@([^\/]*)\/(.*)/);
+        var s = ['sqlcmd -U ', ss[2], ' -S ', ss[4], ' -P ', ss[3], ' -d ', ss[5],
                     ' -Q \"SET NOCOUNT ON;', p, '\" -h-1'].join('');
         cp.exec(s, cb);
     },
@@ -37,13 +40,4 @@ var cnx = {
     }
 }
 
-//cnx.query('http://google.com');
-//cnx.query('http://dpsw.info/echo/', {hello:'world'});
-//cnx.query('cmd://ls', function(e,r){ console.log('cmd', r.length > 10) });
-
-//cnx.query('mysql://user:pass@host/cdrnet_test', 'select 1;');
-//cnx.query('sqlserver://user:password@host/database', 'select 1;');
-//cnx.query('mongo://user:password@host/database', function(db){
-//    db.collection('test').find().toArray(console.log);
-//});
-//cnx.query('redis://user:password@host/database', function(c){});
+module.exports = cnx;
